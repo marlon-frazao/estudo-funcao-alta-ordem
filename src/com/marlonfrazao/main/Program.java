99756204876fa793bc95be47feb3e45ee1e23cdb
marlon-frazao <frazao.marlon@gmail.com>
2021-08-02 12:53:02 -0300

    Final commit

--- src/com/marlonfrazao/main/Program.java
+++ src/com/marlonfrazao/main/Program.java
@@ -4,19 +4,19 @@ public class Program {
 
 	public static void main(String[] args) {
 		Calculo soma = (a, b) -> a + b;		
-		System.out.println("Retorna soma 4 + 2: " +executarOperação(soma, 4, 2));
+		System.out.println("Retorna soma 4 + 2: " +executarOperacao(soma, 4, 2));
 		
 		Calculo subtracao = (a, b) -> a - b;
-		System.out.println("Retorna subtração 4 - 2: " +executarOperação(subtracao, 4, 2));
+		System.out.println("Retorna subtração 4 - 2: " +executarOperacao(subtracao, 4, 2));
 		
 		Calculo multiplicacao = (a, b) -> a * b;
-		System.out.println("Retorna multiplicação 4 * 2: " +executarOperação(multiplicacao, 4, 2));
+		System.out.println("Retorna multiplicação 4 * 2: " +executarOperacao(multiplicacao, 4, 2));
 		
 		Calculo divisao = (a, b) -> a / b;
-		System.out.println("Retorna divisão 4 * 2: " +executarOperação(divisao, 4, 2));
+		System.out.println("Retorna divisão 4 / 2: " +executarOperacao(divisao, 4, 2));
 	}
 	
-	public static Integer executarOperação(Calculo calculo, Integer a, Integer b) {
+	public static Integer executarOperacao(Calculo calculo, Integer a, Integer b) {
 		return calculo.calcular(a, b);
 	}
 }
